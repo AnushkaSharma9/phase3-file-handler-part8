8462673d2e9d6b23e53a647f2ff804bff7e2b89c
Anushka Sharma <anushkasharma2109@gmail.com>
2021-10-19 17:40:25 +0530

    added file handler

---
+++ src/main/java/com/simplilearn/fileserver/Phase3FileHandlingServerPart8Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn.fileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase3FileHandlingServerPart8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase3FileHandlingServerPart8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/fileserver/Phase3FileHandlingServerPart8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.fileserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phase3FileHandlingServerPart8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
